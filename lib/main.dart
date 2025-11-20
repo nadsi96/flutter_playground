f84490a968f83e29c061faec58f1f77668f04f29
nahp <nahp@smsoftlab.co.kr>
2025-11-20 17:45:01 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/riverpod/riverpod_counter_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: routes
+    );
+  }
+}
+
+final routes = {
+  '/home': (context) => Home(),
+};
+class Home extends StatelessWidget{
+
+  Widget menuButton(context, sRoute){
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, sRoute);
+      },
+      child: Container(
+        width: 200,
+        height: 50,
+        color: Colors.blue,
+        child: Center(
+          child: Text("$sRoute")
+        )
+      )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 8,
+            children: [
+
+            ],
+          ),
+        )
+      )
+    );
+  }
+
+}
