6c74076deacb42f1c478698df787e10a8d6e6f74
nadsi96 <nadsi96@naver.com>
2025-12-10 14:51:46 +0900

    bitsdojo_window 위한 windows, macos 설정 리눅스는 설정 안함 필요하면 확인

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,7 +1,21 @@
 import Cocoa
 import FlutterMacOS
+// bitsdojo >>
+/**
+ bitsdojo_window_macos import 추가
+ MainFlutterWindow 상속 NSWindow -> BitsdojoWindow
+ bitsdojo_window_configure() 함수 추가
+ */
+import bitsdojo_window_macos
 
-class MainFlutterWindow: NSWindow {
+
+class MainFlutterWindow: BitsdojoWindow {
+    
+    override func bitsdojo_window_configure() -> UInt {
+      return BDW_HIDE_ON_STARTUP
+    }
+//class MainFlutterWindow: NSWindow {
+// << bitsdojo
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
